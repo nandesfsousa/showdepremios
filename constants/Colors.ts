99596008b1a3a313cc1c97e73d379dc1bf36790a
nandesfsousa <nandesfsousa@outlook.com>
2021-08-15 09:43:33 -0300

    initial commit

---
+++ constants/Colors.ts
@@ -0,0 +1,19 @@
+const tintColorLight = '#1c8d1aff';
+const tintColorDark = '#1c8d1aff';
+
+export default {
+  light: {
+    text: '#000',
+    background: '#fff',
+    tint: tintColorLight,
+    tabIconDefault: '#ccc',
+    tabIconSelected: tintColorLight,
+  },
+  dark: {
+    text: '#fff',
+    background: '#000',
+    tint: tintColorDark,
+    tabIconDefault: '#ccc',
+    tabIconSelected: tintColorDark,
+  },
+};
---
+++ types.tsx
@@ -0,0 +1,24 @@
+/**
+ * Learn more about using TypeScript with React Navigation:
+ * https://reactnavigation.org/docs/typescript/
+ */
+
+export type RootStackParamList = {
+  Root: undefined;
+  NotFound: undefined;
+};
+
+export type BottomTabParamList = {
+  Show: undefined;
+  Events: undefined;
+  Cupons: undefined;
+  Profile: undefined;
+};
+
+export type TabOneParamList = {
+  TabOneScreen: undefined;
+};
+
+export type TabTwoParamList = {
+  TabTwoScreen: undefined;
+};
